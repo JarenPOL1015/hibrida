6a0136e2d6442cf1ef8cb74b1c43b2d933329b5a
JarenPOL1015 <jarapazm@espol.edu.ec>
2024-12-09 10:16:56 -0500

    Firebase

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,17 +3,31 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, Io
   IonSelect, IonSelectOption, IonTextarea,IonButton,
   IonList, IonItem, IonLabel, } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
-    IonSelect, IonSelectOption, IonTextarea,IonButton, IonList, IonItem, IonLabel,]
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+    IonSelect, IonSelectOption, IonTextarea,IonButton, IonList, IonItem, IonLabel, ReactiveFormsModule
+  ]
 })
 export class Tab2Page {
 
   constructor() {}
 
+  myForm: FormGroup = new FormGroup({
+    score: new FormControl("",Validators.required),
+    opinion: new FormControl("",Validators.required)
+  });
+
+  /* El método onSubmit para enviar los datos del formulario mediante el servicio */
+  onSubmit() {
+    console.log(this.myForm.value);
+    alert(this.myForm.controls["score"].value);
+    this.myForm.reset();
+  }  
 }
